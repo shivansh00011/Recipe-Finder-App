97127c729ba6b7fd91b19fde754f53d4b18c2f79
Shivansh Yadav <105140714+shivansh00011@users.noreply.github.com>
2024-11-15 11:03:24 +0530

    Updated

--- lib/constants/constant_function.dart
+++ lib/constants/constant_function.dart
@@ -3,8 +3,8 @@
 import 'package:http/http.dart' as http;
  class ConstantFunction{
   static Future<List<Map<String, dynamic>>> getResponse(String findRecipe) async{
-    String id='a805ba0b';
-    String key='09f733a81e51d1e3daae7a4531d91e3c';
+    String id='YOUR_API_ID';
+    String key='YOUR_API_KEY';
     String api ='https://api.edamam.com/search?q=$findRecipe&app_id=$id&app_key=$key&from=0&to=3&calories=591-722&health=alcohol-free';
     final response  = await http.get(Uri.parse(api));
     List<Map<String, dynamic>> recipe = [];
